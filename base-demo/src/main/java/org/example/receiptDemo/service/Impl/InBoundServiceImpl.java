c70948a7a1dbcfb0ee365d840867c83653c85574
sjc的鱼塘 <1640867217@qq.com>
2024-11-06 10:44:18 +0800

    开始生成入库单

---
+++ base-demo/src/main/java/org/example/receiptDemo/service/Impl/InBoundServiceImpl.java
@@ -0,0 +1,8 @@
+package org.example.receiptDemo.service.Impl;
+
+import org.example.receiptDemo.service.InBoundService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InBoundServiceImpl implements InBoundService {
+}
---
+++ base-demo/src/main/java/org/example/receiptDemo/service/InBoundService.java
@@ -0,0 +1,4 @@
+package org.example.receiptDemo.service;
+
+public interface InBoundService {
+}
